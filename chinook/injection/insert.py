c2eb637b868d916e2ee41d8faf41b5ab5c6223dd
irisg648 <irisgonzalez648@gmail.com>
2020-05-17 21:01:39 +0000

    Changed insert.py

--- chinook/injection/insert.py
+++ chinook/injection/insert.py
@@ -6,7 +6,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("db", default=":memory:", help="The file path of the database to be edited")
-parser.add_argument("-r", "--roster", help="Inserts names from ROSTER into db. ROSTER should be a plain-text file with one name on each line.")
+parser.add_argument("-r", "--roster")
 args = parser.parse_args()
 
 # ===== END CLI BLOCK ===== #
@@ -18,13 +18,15 @@ if args.roster:
     with open(args.roster) as f:
         for record in f:
             name = record.strip()
-            cur.executescript(f"INSERT INTO students (name) values ('{name}')")
+            cur.execute(f"INSERT INTO students (name) VALUES(?)", (name,))
             conn.commit()
         conn.close()
-
+        
 else:
     while True:
         name = input("Type your first and last name and hit enter to register. Hit Ctrl+C to quit.\n> ")
-        cur.execute(f"INSERT INTO students (name) VALUES ('{name}');")
+        cur.execute(f"INSERT INTO students (name) VALUES (?)", (name,))
         conn.commit()
+
+
 
